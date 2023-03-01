471a0e044d710a373d22d83f6d54bf654b98644c
Tomás Antunes <73246184+tomassantunes@users.noreply.github.com>
2023-03-01 20:43:31 +0000

    added cli app, flags, usage for flags
    scan of 1024 tcp and udp ports, or scan of individual port
    
    --ip <ip>
    --pc <protocol tcp or udp>
    --p <port>
    --t <timeout in milliseconds>

--- main.go
+++ main.go
@@ -2,8 +2,58 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/tomassantunes/port-scanner/port"
+	"github.com/urfave/cli"
 )
 
 func main() {
-	fmt.Println("Port Scanner in Go")
+	var ip string
+	var pc string // protocol
+	var p int     // port
+
+	app := &cli.App{
+		Name:   "Port Scanner",
+		Usage:  "Scan for open ports",
+		Author: "Tomás Antunes - @tomassantunes",
+	}
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "ip",
+			Usage:       "e.g --ip 127.0.0.1/12, --ip localhost",
+			Required:    true,
+			Destination: &ip,
+		},
+		cli.StringFlag{
+			Name:        "protocol, pc",
+			Value:       "tcp, udp",
+			Usage:       "Protocol for IP(s) scan, e.g --pc tcp",
+			Destination: &pc,
+		},
+		cli.IntFlag{
+			Name:        "port, p",
+			Usage:       "Port to scan, e.g --p 200",
+			Destination: &p,
+		},
+		cli.StringFlag{
+			Name:  "timeout, t",
+			Value: "2000",
+			Usage: "Timeout in milliseconds, e.g --t 3000",
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+
+	if len(pc) > 0 && p > 0 {
+		open := port.ScanPort(pc, ip, p)
+		fmt.Printf("Port %s/%d: %s\n", pc, p, open.State)
+	}
+
+	results := port.InitialScan("localhost")
+	fmt.Println(results)
 }
